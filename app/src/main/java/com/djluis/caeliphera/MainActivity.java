a1efadf14f4c9a1a7d66e988b7e874f444481ff7
Luis Suárez <xboxluis2010@hotmail.com>
2022-06-28 22:42:02 -0500

    Se agrego campos al activity login

--- app/src/main/java/com/djluis/caeliphera/MainActivity.java
+++ app/src/main/java/com/djluis/caeliphera/MainActivity.java
@@ -1,5 +1,6 @@
 package com.djluis.caeliphera;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,5 +10,6 @@ public class MainActivity extends AppCompatActivity {
   @Override protected void onCreate (Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+    startActivity(new Intent(this, LoginActivity.class));
   }
 }
