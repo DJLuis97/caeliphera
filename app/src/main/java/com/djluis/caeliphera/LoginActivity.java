c693dfadaabdc50520b413421dc2d4f87477567b
Luis Suárez <xboxluis2010@hotmail.com>
2022-07-01 08:39:23 -0500

    Se implemento login con la API

--- app/src/main/java/com/djluis/caeliphera/LoginActivity.java
+++ app/src/main/java/com/djluis/caeliphera/LoginActivity.java
@@ -1,21 +1,24 @@
 package com.djluis.caeliphera;
 
-import android.annotation.SuppressLint;
+import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.android.volley.NetworkResponse;
 import com.android.volley.Request;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
-import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
@@ -25,6 +28,7 @@ import java.util.Map;
 public class LoginActivity extends AppCompatActivity {
   private EditText text_username_login, text_password_login;
   private Button btn_login_login;
+  TextView helper_username_login, helper_password_login;
 
   @Override protected void onCreate (Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
@@ -32,40 +36,93 @@ public class LoginActivity extends AppCompatActivity {
     text_password_login = findViewById(R.id.text_password_login);
     text_username_login = findViewById(R.id.text_username_login);
     btn_login_login = findViewById(R.id.btn_login_login);
+    helper_username_login = findViewById(R.id.helper_username_login);
+    helper_password_login = findViewById(R.id.helper_password_login);
     btn_login_login.setOnClickListener(new View.OnClickListener() {
       @Override public void onClick (View view) {
-        loginAction();
+        loginAction(
+            text_username_login.getText().toString(), text_password_login.getText().toString());
       }
     });
   }
 
-  private void loginAction () {
-    String url = "https://caeliphera-api.herokuapp.com/api/auth/login";
-    StringRequest sr = new StringRequest(Request.Method.POST, url, new Response.Listener<String>() {
-      @Override public void onResponse (String response) {
-        Toast.makeText(LoginActivity.this, "response", Toast.LENGTH_SHORT).show();
-        try {
-          JSONObject json = new JSONObject(response);
-          Toast.makeText(LoginActivity.this, response, Toast.LENGTH_SHORT).show();
-        } catch (JSONException e) {
-          e.printStackTrace();
-        }
+  private void loginAction (String email, String password) {
+    cleanHelpers();
+    String              url    = "https://caeliphera-api.herokuapp.com/api/auth/login";
+    Map<String, String> params = new HashMap<>();
+    params.put("email", email);
+    params.put("password", password);
+    params.put("device_name", Build.MODEL);
+    JsonObjectRequest jor = new JsonObjectRequest(
+        Request.Method.POST, url, new JSONObject(params), new Response.Listener<JSONObject>() {
+      @Override public void onResponse (JSONObject response) {
+        Log.e(
+            "loginAction", "(╯°□°）╯︵ ┻━┻ |> " + response.toString());
       }
     }, new Response.ErrorListener() {
       @Override public void onErrorResponse (VolleyError error) {
-        Toast.makeText(LoginActivity.this, "error response", Toast.LENGTH_SHORT).show();
-        Log.e("error loginAction", error.getMessage());
+        String          json     = null;
+        NetworkResponse response = error.networkResponse;
+        if (response != null && response.data != null) {
+          json = new String(response.data);
+          String message = getJson(json, "message");
+          if (message != null) {
+            Toast.makeText(LoginActivity.this, message, Toast.LENGTH_SHORT).show();
+          }
+          switch (response.statusCode) {
+            case 422:
+              try {
+                String errors = getJson(json, "errors");
+                JSONArray error_email = getJsonArray(
+                    errors, "email"), error_password = getJsonArray(errors, "password");
+                helper_username_login.setText(error_email.getString(0));
+                helper_password_login.setText(error_password.getString(0));
+              } catch (JSONException e) {
+                e.printStackTrace();
+              } catch (Exception e) {
+                e.printStackTrace();
+              }
+              break;
+            default:
+              Toast.makeText(LoginActivity.this, "Unknown Server Error", Toast.LENGTH_SHORT).show();
+          }
+        } else {
+          Log.e("loginAction", "(╯°□°）╯︵ ┻━┻ |> " + error.toString());
+        }
       }
     })
     {
-      protected Map<String, String> getParams () {
-        Map<String, String> params = new HashMap<>();
-        params.put("email", text_username_login.getText().toString());
-        params.put("password", text_password_login.getText().toString());
-
-        return params;
+      public Map<String, String> getHeaders () {
+        Map<String, String> headers = new HashMap<String, String>();
+        headers.put("Accept", "application/json");
+        headers.put("Content-Type", "application/json");
+        return headers;
       }
     };
-    Volley.newRequestQueue(this).add(sr);
+    Volley.newRequestQueue(this).add(jor);
+  }
+
+  private String getJson (String json, String key) {
+    try {
+      JSONObject jo = new JSONObject(json);
+      return jo.getString(key);
+    } catch (JSONException e) {
+      e.printStackTrace();
+      return null;
+    }
+  }
+
+  private JSONArray getJsonArray (String json, String key) {
+    try {
+      JSONObject jo = new JSONObject(json);
+      return jo.getJSONArray(key);
+    } catch (JSONException e) {
+      e.printStackTrace();
+      return null;
+    }
+  }
+
+  private void cleanHelpers () {
+    helper_username_login.setText("");
   }
 }
