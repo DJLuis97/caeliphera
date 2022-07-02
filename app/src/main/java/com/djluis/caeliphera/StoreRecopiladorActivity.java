57c9269c274484fc464791767fbf0cd44840f2a9
Luis Suárez <xboxluis2010@hotmail.com>
2022-07-02 15:41:28 -0500

    Se rellena spinner con las parroquias de la API

--- app/src/main/java/com/djluis/caeliphera/StoreRecopiladorActivity.java
+++ app/src/main/java/com/djluis/caeliphera/StoreRecopiladorActivity.java
@@ -1,39 +1,121 @@
 package com.djluis.caeliphera;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
+import android.widget.Spinner;
 import android.widget.Toast;
 
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.NetworkResponse;
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+import com.djluis.caeliphera.entities.Parroquia;
+import com.djluis.caeliphera.io.FileHelper;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class StoreRecopiladorActivity extends AppCompatActivity {
+	private EditText txt_ci_recopilador, txt_first_name_recopilador, text_last_name_recopilador;
+	private Spinner sp_parroquia_recopilador;
 
-  private Button   btn_back_store_recopilador, btn_store_recopilador;
-  private EditText txt_ci_recopilador, txt_first_name_recopilador, text_last_name_recopilador;
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_store_recopilador);
+		setUpView();
+		List<Parroquia> lp = llenar();
+		ArrayAdapter<Parroquia> aap = new ArrayAdapter<>(getApplicationContext(),
+				com.google.android.material.R.layout.support_simple_spinner_dropdown_item,
+				lp
+		);
+		sp_parroquia_recopilador.setAdapter(aap);
+	}
 
-  @Override protected void onCreate (Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_store_recopilador);
-    btn_back_store_recopilador = (Button) findViewById(R.id.btn_back_store_recopilador);
-    text_last_name_recopilador = (EditText) findViewById(R.id.text_last_name_recopilador);
-    txt_ci_recopilador = (EditText) findViewById(R.id.txt_ci_recopilador);
-    txt_first_name_recopilador = (EditText) findViewById(R.id.txt_first_name_recopilador);
-    btn_store_recopilador = (Button) findViewById(R.id.btn_store_recopilador);
+	private List<Parroquia> llenar () {
+		List<Parroquia> parroquias = new ArrayList<>();
+		try {
+			String token = FileHelper.getToken(openFileInput("token.txt"));
+			if (token == null) throw new RuntimeException("Token nulo.");
+			String url = "https://caeliphera-api.herokuapp.com/api/v1/parroquias";
+			JsonObjectRequest jor = new JsonObjectRequest(Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+				@Override
+				public void onResponse (JSONObject response) {
+					Log.d("data => ", response.toString());
+					try {
+						JSONArray ja = response.getJSONArray("parroquias");
+						for (int i = 0; i < ja.length(); i++) {
+							Parroquia p = new Parroquia();
+							p.setId(ja.getJSONObject(i).getInt("id"));
+							p.setName(ja.getJSONObject(i).getString("name"));
+							parroquias.add(p);
+						}
+					} catch (JSONException e) {
+						e.printStackTrace();
+					}
+				}
+			}, new Response.ErrorListener() {
+				@Override
+				public void onErrorResponse (VolleyError error) {
+					NetworkResponse response = error.networkResponse;
+					if (response != null && response.data != null) {
+						Toast.makeText(StoreRecopiladorActivity.this,
+								"Error de respuesta: " + response.statusCode,
+								Toast.LENGTH_LONG
+						).show();
+					} else {
+						Log.e("(╯°□°）╯︵ ┻━┻ |>", "StoreRecopiladorActivity@llenar#onErrorResponse -> " + error.toString());
+					}
+				}
+			}) {
+				@Override
+				public Map<String, String> getHeaders () throws AuthFailureError {
+					Map<String, String> headers = new HashMap<String, String>();
+					headers.put("Accept", "application/json");
+					headers.put("Authorization", "Bearer " + token);
+					return headers;
+				}
+			};
+			Volley.newRequestQueue(this).add(jor);
+		} catch (FileNotFoundException exception) {
+			Log.e("(╯°□°）╯︵ ┻━┻ |>", "StoreRecopiladorActivity@llenar -> " + exception.getMessage());
+		}
+		return parroquias;
+	}
 
-    btn_back_store_recopilador.setOnClickListener(new View.OnClickListener() {
-      @Override public void onClick (View view) {
-        startActivity(new Intent(StoreRecopiladorActivity.this, MainActivity.class));
-      }
-    });
-
-    btn_store_recopilador.setOnClickListener(new View.OnClickListener() {
-      @Override public void onClick (View view) {
-        Toast.makeText(StoreRecopiladorActivity.this, "Recopilador Guardado", Toast.LENGTH_SHORT).show();
-      }
-    });
-  }
+	private void setUpView () {
+		text_last_name_recopilador = (EditText) findViewById(R.id.text_last_name_recopilador);
+		txt_ci_recopilador = (EditText) findViewById(R.id.txt_ci_recopilador);
+		txt_first_name_recopilador = (EditText) findViewById(R.id.txt_first_name_recopilador);
+		sp_parroquia_recopilador = (Spinner) findViewById(R.id.sp_parroquia_recopilador);
+		((Button) findViewById(R.id.btn_back_store_recopilador)).setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick (View view) {
+				startActivity(new Intent(StoreRecopiladorActivity.this, MainActivity.class));
+			}
+		});
+		((Button) findViewById(R.id.btn_store_recopilador)).setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick (View view) {
+				Toast.makeText(StoreRecopiladorActivity.this, "Recopilador Guardado", Toast.LENGTH_SHORT).show();
+			}
+		});
+	}
 }
--- app/src/main/java/com/djluis/caeliphera/entities/Parroquia.java
+++ app/src/main/java/com/djluis/caeliphera/entities/Parroquia.java
@@ -1,22 +1,29 @@
 package com.djluis.caeliphera.entities;
 
+import androidx.annotation.NonNull;
+
 public class Parroquia {
-  private int    id;
-  private String name;
+	private int    id;
+	private String name;
 
-  public int getId () {
-    return id;
-  }
+	public int getId () {
+		return id;
+	}
 
-  public void setId (int id) {
-    this.id = id;
-  }
+	public void setId (int id) {
+		this.id = id;
+	}
 
-  public String getName () {
-    return name;
-  }
+	public String getName () {
+		return name;
+	}
 
-  public void setName (String name) {
-    this.name = name;
-  }
+	public void setName (String name) {
+		this.name = name;
+	}
+
+	@NonNull
+	public String toString () {
+		return id + "-" + name;
+	}
 }
