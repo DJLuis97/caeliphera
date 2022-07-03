464010d456a4eaa4e0364e398f53912f3a28dc9a
Luis Suárez <xboxluis2010@hotmail.com>
2022-07-03 11:45:54 -0500

    Se creó la entidad `PersonRecopilador`

---
+++ app/src/main/java/com/djluis/caeliphera/entities/PersonRecopilador.java
@@ -0,0 +1,13 @@
+package com.djluis.caeliphera.entities;
+
+public class PersonRecopilador extends Person {
+	protected int id_recopilador;
+
+	public int getIdRecopilador () {
+		return id_recopilador;
+	}
+
+	public void setIdRecopilador (int id_encargado) {
+		this.id_recopilador = id_encargado;
+	}
+}
