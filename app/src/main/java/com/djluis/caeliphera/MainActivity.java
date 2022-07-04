ad8ffa0de6fe93a266dcaa4e35fcbe3adb8c6496
Luis Suárez <xboxluis2010@hotmail.com>
2022-07-04 07:56:15 -0500

    Se agregó progress bar al cerrar sesión

--- app/src/main/java/com/djluis/caeliphera/MainActivity.java
+++ app/src/main/java/com/djluis/caeliphera/MainActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -23,20 +24,25 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
-	private TextView welcome_main;
+	private TextView    welcome_main;
+	private ProgressBar progress_bar_main;
+	private Button      btn_go_store_recopilador, btn_logout;
 
 	@Override
 	protected void onCreate (Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		welcome_main = (TextView) findViewById(R.id.welcome_main);
-		((Button) findViewById(R.id.btn_go_store_recopilador)).setOnClickListener(new View.OnClickListener() {
+		progress_bar_main = (ProgressBar) findViewById(R.id.progress_bar_main);
+		btn_go_store_recopilador = (Button) findViewById(R.id.btn_go_store_recopilador);
+		btn_logout = (Button) findViewById(R.id.btn_logout);
+		btn_go_store_recopilador.setOnClickListener(new View.OnClickListener() {
 			@Override
 			public void onClick (View view) {
 				startActivity(new Intent(MainActivity.this, StoreRecopiladorActivity.class));
 			}
 		});
-		((Button) findViewById(R.id.btn_logout)).setOnClickListener(new View.OnClickListener() {
+		btn_logout.setOnClickListener(new View.OnClickListener() {
 			@Override
 			public void onClick (View view) {
 				logout();
@@ -49,7 +55,20 @@ public class MainActivity extends AppCompatActivity {
 		}
 	}
 
+	private void showLoading () {
+		progress_bar_main.setVisibility(View.VISIBLE);
+		btn_logout.setEnabled(false);
+		btn_go_store_recopilador.setEnabled(false);
+	}
+
+	private void hideLoading () {
+		progress_bar_main.setVisibility(View.GONE);
+		btn_logout.setEnabled(true);
+		btn_go_store_recopilador.setEnabled(true);
+	}
+
 	private void logout () {
+		showLoading();
 		String url = "https://caeliphera-api.herokuapp.com/api/auth/logout", token;
 		try {
 			token = FileHelper.getToken(openFileInput("token.txt"));
@@ -57,6 +76,7 @@ public class MainActivity extends AppCompatActivity {
 			StringRequest sr = new StringRequest(Request.Method.GET, url, new Response.Listener<String>() {
 				@Override
 				public void onResponse (String response) {
+					hideLoading();
 					File token_file = new File(getFilesDir(), "token.txt");
 					if (token_file.delete()) {
 						Toast.makeText(MainActivity.this, response, Toast.LENGTH_SHORT).show();
@@ -68,6 +88,7 @@ public class MainActivity extends AppCompatActivity {
 			}, new Response.ErrorListener() {
 				@Override
 				public void onErrorResponse (VolleyError error) {
+					hideLoading();
 					Toast.makeText(MainActivity.this, "Error API al cerrar sesión :(", Toast.LENGTH_LONG).show();
 					Log.e("(╯°□°）╯︵ ┻━┻ |>", "MainActivity@logout#onErrorResponse -> " + error.toString());
 				}
@@ -82,6 +103,7 @@ public class MainActivity extends AppCompatActivity {
 			};
 			Volley.newRequestQueue(MainActivity.this).add(sr);
 		} catch (FileNotFoundException exception) {
+			hideLoading();
 			Log.e("(╯°□°）╯︵ ┻━┻ |>", "MainActivity@logout -> " + exception.getMessage());
 			Toast.makeText(this, "Error al intentar cerrar sesión", Toast.LENGTH_LONG).show();
 		}
